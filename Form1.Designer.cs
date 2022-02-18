798b3c23b3308bc101ba2dd9ef366d113eb7116d
Максим Казанцев <m.p.rybakov@gmail.com>
2022-02-18 23:10:58 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Half_interval
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+			this.button2 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(639, 112);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(260, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Уточнить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.DecimalPlaces = 3;
+			this.numericUpDown1.Location = new System.Drawing.Point(726, 11);
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(173, 27);
+			this.numericUpDown1.TabIndex = 13;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			// 
+			// numericUpDown2
+			// 
+			this.numericUpDown2.DecimalPlaces = 3;
+			this.numericUpDown2.Location = new System.Drawing.Point(726, 46);
+			this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+			this.numericUpDown2.Name = "numericUpDown2";
+			this.numericUpDown2.Size = new System.Drawing.Size(173, 27);
+			this.numericUpDown2.TabIndex = 2;
+			this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+			this.pictureBox1.TabIndex = 3;
+			this.pictureBox1.TabStop = false;
+			// 
+			// numericUpDown3
+			// 
+			this.numericUpDown3.DecimalPlaces = 10;
+			this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+			this.numericUpDown3.Location = new System.Drawing.Point(726, 79);
+			this.numericUpDown3.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown3.Name = "numericUpDown3";
+			this.numericUpDown3.Size = new System.Drawing.Size(172, 27);
+			this.numericUpDown3.TabIndex = 5;
+			this.numericUpDown3.ThousandsSeparator = true;
+			this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(638, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(82, 20);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "Максимум";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(638, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(78, 20);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "Минимум";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(638, 79);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(65, 20);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "Ошибка";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(639, 298);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(72, 20);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Масштаб";
+			// 
+			// numericUpDown4
+			// 
+			this.numericUpDown4.Location = new System.Drawing.Point(726, 298);
+			this.numericUpDown4.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+			this.numericUpDown4.Name = "numericUpDown4";
+			this.numericUpDown4.Size = new System.Drawing.Size(172, 27);
+			this.numericUpDown4.TabIndex = 10;
+			this.numericUpDown4.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(638, 331);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(260, 40);
+			this.button2.TabIndex = 11;
+			this.button2.Text = "Рисовать";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.HorizontalScrollbar = true;
+			this.listBox1.ItemHeight = 20;
+			this.listBox1.Location = new System.Drawing.Point(639, 158);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(260, 124);
+			this.listBox1.TabIndex = 12;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(638, 586);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(260, 27);
+			this.textBox1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1011, 632);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.numericUpDown4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown3);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.numericUpDown2);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.NumericUpDown numericUpDown2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.NumericUpDown numericUpDown4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Half_interval
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		decimal F(decimal x)
+		{
+			try
+			{
+				//return (decimal)Math.Sqrt((double)x);
+				//return x*x*x-2*x-5;
+				return (decimal)Math.Sin((double)x);
+				//return x;
+				//return (decimal)((double)x + Math.Pow(Math.E, (double)x));
+			}
+			catch (Exception)
+			{
+				return 0m;
+			}
+		}
+
+		float F(float x)
+		{
+			try
+			{
+				return (float)F((decimal)x);
+			}
+			catch (Exception)
+			{
+				return 0f;
+			}
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			decimal min = numericUpDown1.Value;
+			decimal max = numericUpDown2.Value;
+			if (min > max)
+			{
+				min = min + max;
+				max = min - max;
+				min = min - max;
+			}
+
+			decimal? min_val = null,
+				max_val = null;
+
+			decimal error = numericUpDown3.Value;
+			int c = 0;
+			while (max - min >= error)
+			{
+				c++;
+				min_val ??= F(min);
+				max_val ??= F(max);
+
+				decimal avg = (min + max) / 2M,
+					avg_val = F(avg);
+
+				if (min_val * avg_val < 0)
+				{
+					max = avg;
+					max_val = avg_val;
+				}
+				else
+				{
+					min = avg;
+					min_val = avg_val;
+				}
+
+				if (c > 100000)
+					break;
+			}
+
+
+			string format = $"F{CalcDigits(error)}";
+			listBox1.Items.Insert(0, "");
+			listBox1.Items.Insert(0, $"шагов: {c}");
+			listBox1.Items.Insert(0, $"ξ ϵ ({min.ToString(format)};{max.ToString(format)})");
+			textBox1.Text = $"ξ ϵ ({min.ToString(format)};{max.ToString(format)})";
+
+		}
+
+		int CalcDigits(decimal num)
+		{
+			int i;
+			for (i = 1; i < 15; i++)
+			{
+				num *= 10;
+				if (num >= 1)
+					break;
+			}
+
+			return i;
+		}
+
+		int MAX_REAL = 10;
+		int MIN_REAL;
+		int NUM_STEPS = 100;
+		const int UNIT_OFFSET = 2;
+		const int FITH_UNIT_OFFSET = 6;
+		float ratio;
+
+		Pen gridPen = new(Color.Black, 1)
+		{
+			Alignment = PenAlignment.Center,
+			StartCap = LineCap.Round,
+			EndCap = LineCap.Round
+		};
+		Pen graphPen = new(Color.Blue, 1)
+		{
+			Alignment = PenAlignment.Center,
+			StartCap = LineCap.Round,
+			EndCap = LineCap.Round
+		};
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			MAX_REAL = (int)numericUpDown4.Value;
+			MIN_REAL = -MAX_REAL;
+
+			#region bitmap stuff
+			Bitmap mainBmp;
+			Graphics graphics;
+			RectangleF pictureBoxBounds;
+			gridPen.Alignment = PenAlignment.Center;
+			gridPen.StartCap = gridPen.EndCap = LineCap.Round;
+			using (Graphics g = pictureBox1.CreateGraphics())
+			{
+				pictureBoxBounds = g.VisibleClipBounds;
+				mainBmp = new Bitmap((int)pictureBoxBounds.Width, (int)pictureBoxBounds.Height, g);
+			}
+			graphics = Graphics.FromImage(mainBmp);
+			graphics.SmoothingMode = SmoothingMode.AntiAlias;
+			#endregion
+
+			// pictureBox check
+			if ((int)pictureBoxBounds.Width != (int)pictureBoxBounds.Height)
+			{
+				throw new Exception("height should be equal to width");
+			}
+
+			int pbSize = (int)pictureBoxBounds.Width;
+			int pbC = pbSize / 2;
+			ratio = MAX_REAL * 2f / pbSize;
+
+			#region draw grid
+			gridPen.Width = 1;
+			graphics.DrawLine(gridPen, pbSize / 2, 0, pbSize / 2, pbSize);
+			graphics.DrawLine(gridPen, 0, pbSize / 2, pbSize, pbSize / 2);
+
+			gridPen.Width = 1;
+			for (float realX = MIN_REAL; realX <= MAX_REAL; realX += 1)
+			{
+				int x = (int)ToCord(realX, ratio, pbSize);
+
+				int offset;
+				if (realX % 5 == 0)
+					offset = FITH_UNIT_OFFSET;
+				else
+					offset = UNIT_OFFSET;
+
+				graphics.DrawLine(gridPen, x, pbC - offset, x, pbC + offset);
+				graphics.DrawLine(gridPen, pbC - offset, x, pbC + offset, x);
+			}
+			#endregion
+
+			List<PointF> graphPoints = new();
+			for (float x = 0; x < pbSize; x += pbSize / NUM_STEPS)
+			{
+				float correctedX = ToReal(x, ratio, MAX_REAL),
+				 y = F(correctedX),
+				 reversedY = ToCord(y, ratio, pbSize);
+
+				graphPoints.Add(new PointF(x, reversedY));
+			}
+
+			graphics.DrawCurve(graphPen, graphPoints.ToArray());
+
+			pictureBox1.Image = mainBmp;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button2_Click(sender, null);
+		}
+
+		float ToCord(float real, float ratio, float pic_dimension)
+		{
+			return MathF.Max(MathF.Min((-real) / ratio + pic_dimension / 2, pic_dimension), 0);
+		}
+
+		float ToReal(float cord, float ratio, float max_real)
+		{
+			return cord * ratio - max_real;
+		}
+	}
+}
